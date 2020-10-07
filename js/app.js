b5638c76397f910a35d04d61e4f16d6552d74284
endiawilliams <endiawilliams1@gmail.com>
2020-10-07 17:05:35 -0500

    Replaced temporary array with count variable inside getBombs

--- js/app.js
+++ js/app.js
@@ -5,20 +5,19 @@ let allColumns = [
     [], [], [], []
 ]
 
-let tempArray = []
-
 function generateBombs () {
-    while (tempArray.length < 40) {
+    let count = 0
+    while (count < 40) {
         // returns a random number from 0 to 15
         let randomCol = Math.floor(Math.random() * 16)
         let randomIndex = Math.floor(Math.random() * 16)
 
         if (allColumns[randomCol].includes(randomIndex) === false) {
-            tempArray.push(`${randomCol} + ${randomIndex}`)
             allColumns[randomCol].push(randomIndex)
+            count += 1
 
             let targetCol = document.getElementById(`col${randomCol}`)
-            // targetCol.querySelector(`.square${randomIndex}`).innerHTML = B
+            targetCol.querySelector(`.square${randomIndex}`).innerHTML = 0
         }
     }
 }
@@ -26,5 +25,13 @@ function generateBombs () {
 generateBombs()
 
 function generateNums () {
-
+    allColumns.forEach(element => {
+        let column = element
+        element.forEach(element => {
+            let bombSquare = element
+            
+        })
+    })
 }
+
+generateNums()
