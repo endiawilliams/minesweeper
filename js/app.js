da6f61874fc3bbe06cd7d69741dcb7620a1c86d3
endiawilliams <endiawilliams1@gmail.com>
2020-10-07 22:46:09 -0500

    Attempted to create function to generate numbers. Now refactoring grid to a single array.

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ function generateBombs () {
             allColumns[randomCol].push(randomIndex)
             count += 1
 
-            let targetCol = document.getElementById(`col${randomCol}`)
-            targetCol.querySelector(`.square${randomIndex}`).innerHTML = 0
+            let columnDiv = document.getElementById(`col${randomCol}`)
+            columnDiv.querySelector(`.square${randomIndex}`).innerHTML = 0
         }
     }
 }
@@ -26,10 +26,28 @@ generateBombs()
 
 function generateNums () {
     allColumns.forEach(element => {
-        let column = element
+        let colArray = element
+        let colArrayIndex = element.indexOf
         element.forEach(element => {
-            let bombSquare = element
+            // typeof returns number for both element and bombSquare
+            // and columnArray[bombIndex] works
+            let bombIndex = element.indexOf
+            let squareAbove = element - 1
+            let colDiv = document.getElementById(`col${colArrayIndex}`)
             
+            // If the above square is empty AND this bomb is not on the top 
+            // row mark as numbered (refactor with ! not operator, not sure 
+            // how to make it work)
+            if (colArray.includes(squareAbove) === false && bombIndex !== 0) {
+                console.log(colDiv.querySelector(`.square1`).innerHTML)
+                console.log(colDiv.querySelector(`.square${squareAbove}`))
+            // If the bomb is on the top row
+            } else if (bombIndex === 0) {
+                
+            // If the the square above the bomb has another bomb
+            } else if (colArray.includes(squareAbove)) {
+                
+            }
         })
     })
 }
