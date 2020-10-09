6a34271236838e3cb1f637b2da6c71c71c5a1ce7
endiawilliams <endiawilliams1@gmail.com>
2020-10-09 00:41:12 -0500

    Added recursive function to reveal adjacent empty and numbered squares, but sometimes getting stuck in a recursive loop

--- js/app.js
+++ js/app.js
@@ -219,66 +219,77 @@ findEmptyCells()
 
 let cellDiv = document.querySelectorAll('.cell')
 
-function displayEmpties () {
-    for (let i = 0; i < emptyCells.length; i++) {
-        let currentIndex = emptyCells[i]
-        console.log(typeof currentIndex)
-        let divAtCurrentIndex = document.querySelector(`#cell${currentIndex}`)
+function revealAdjEmpties (currentIndex) {
+        // let divAtCurrentIndex = document.querySelector(`#cell${currentIndex}`)
 
-        let cellAbove = currentIndex - 16
-        let cellBelow = currentIndex + 16
-        let cellRight = currentIndex + 1
-        let cellLeft = currentIndex - 1
-        let cellTopRight = currentIndex - 15
-        let cellTopLeft = currentIndex - 17
-        let cellBottomRight = currentIndex + 17
-        let cellBottomLeft = currentIndex + 15
-        
-        if (divAtCurrentIndex.classList.contains('visible_cell')) {
-            // If the index is not in the top row and the cell above it is 
-            // included in the emptyCells array, make it display
-            if (currentIndex <= 15 === false && emptyCells.includes(cellAbove)) {
-                
-                // document.querySelector(`#cell${cellAbove}`).classList.remove('evencell', 'oddcell')
-                // document.querySelector(`#cell${cellAbove}`).classList.add('visible_cell')
-            }
-    
-            if (currentIndex >= 240 === false && emptyCells.includes(cellBelow)) {
-                document.querySelector(`#cell${cellBelow}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellBelow}`).classList.add('visible_cell')
-            }
-    
-            if (rightCol.includes(currentIndex) === false && emptyCells.includes(cellRight)) {
-                document.querySelector(`#cell${cellRight}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellRight}`).classList.add('visible_cell')
-            }
+    let cellAbove = currentIndex - 16
+    let cellBelow = currentIndex + 16
+    let cellRight = currentIndex + 1
+    let cellLeft = currentIndex - 1
+    let cellTopRight = currentIndex - 15
+    let cellTopLeft = currentIndex - 17
+    let cellBottomRight = currentIndex + 17
+    let cellBottomLeft = currentIndex + 15
 
-            if (leftCol.includes(currentIndex) === false && emptyCells.includes(cellLeft)) {
-                document.querySelector(`#cell${cellLeft}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellLeft}`).classList.add('visible_cell')
-            }
-    
-            if (rightCol.includes(currentIndex) === false && currentIndex <= 15 === false && emptyCells.includes(cellTopRight)) {
-                document.querySelector(`#cell${cellTopRight}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellTopRight}`).classList.add('visible_cell')
-            }
-    
-            if (leftCol.includes(currentIndex) === false && currentIndex <= 15 === false && emptyCells.includes(cellTopLeft)) {
-                document.querySelector(`#cell${cellTopLeft}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellTopLeft}`).classList.add('visible_cell')
-            }
-    
-            if (rightCol.includes(currentIndex) === false && currentIndex >= 240 === false && emptyCells.includes(cellBottomRight)) {
-                document.querySelector(`#cell${cellBottomRight}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellBottomRight}`).classList.add('visible_cell')
-            }
-    
-            if (leftCol.includes(currentIndex) === false && currentIndex >= 240 === false && emptyCells.includes(cellBottomLeft)) {
-                document.querySelector(`#cell${cellBottomLeft}`).classList.remove('evencell', 'oddcell')
-                document.querySelector(`#cell${cellBottomLeft}`).classList.add('visible_cell')
-            }
-        }
+    // If the index is not in the top row and the cell above it is 
+    // included in the emptyCells array, make it display
+    if (currentIndex <= 15 === false && (emptyCells.includes(cellAbove) || numCells.includes(cellAbove))) {
+        console.log(`We're checking ${currentIndex} for cellAbove which is ${cellAbove}`)
+        document.getElementById(`cell${cellAbove}`).click()
+        // document.querySelector(`#cell${cellAbove}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellAbove}`).classList.add('visible_cell')
     }
+
+    if (currentIndex >= 240 === false && (emptyCells.includes(cellBelow) || numCells.includes(cellBelow))) {
+        console.log(`We're checking ${currentIndex} for cellBelow which is ${cellBelow}`)
+        document.getElementById(`cell${cellBelow}`).click()
+        // document.querySelector(`#cell${cellBelow}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellBelow}`).classList.add('visible_cell')
+    }
+
+    if (rightCol.includes(currentIndex) === false && (emptyCells.includes(cellRight) || numCells.includes(cellRight))) {
+        console.log(`We're checking ${currentIndex} for cellRight which is ${cellRight}`)
+        document.getElementById(`cell${cellRight}`).click()
+        // document.querySelector(`#cell${cellRight}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellRight}`).classList.add('visible_cell')
+    }
+
+    if (leftCol.includes(currentIndex) === false && (emptyCells.includes(cellLeft) || numCells.includes(cellLeft))) {
+        console.log(`We're checking ${currentIndex} for cellLeft which is ${cellLeft}`)
+        document.getElementById(`cell${cellLeft}`).click()
+        // document.querySelector(`#cell${cellLeft}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellLeft}`).classList.add('visible_cell')
+    }
+    
+    if (rightCol.includes(currentIndex) === false && currentIndex <= 15 === false && (emptyCells.includes(cellTopRight) || numCells.includes(cellTopRight))) {
+        console.log(`We're checking ${currentIndex} for cellTopRight which is ${cellTopRight}`)
+        document.getElementById(`cell${cellTopRight}`).click()
+        // document.querySelector(`#cell${cellTopRight}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellTopRight}`).classList.add('visible_cell')
+    }
+
+    if (leftCol.includes(currentIndex) === false && currentIndex <= 15 === false && (emptyCells.includes(cellTopLeft) || numCells.includes(cellTopLeft))) {
+        console.log(`We're checking ${currentIndex} for cellTopLeft which is ${cellTopLeft}`)
+        document.getElementById(`cell${cellTopLeft}`).click()
+        // document.querySelector(`#cell${cellTopLeft}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellTopLeft}`).classList.add('visible_cell')
+    }
+    
+    if (rightCol.includes(currentIndex) === false && currentIndex >= 240 === false && (emptyCells.includes(cellBottomRight) || numCells.includes(cellBottomRight))) {
+        console.log(`We're checking ${currentIndex} for cellBottomRight which is ${cellBottomRight}`)
+        document.getElementById(`cell${cellBottomRight}`).click()
+        // document.querySelector(`#cell${cellBottomRight}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellBottomRight}`).classList.add('visible_cell')
+    }
+
+    if (leftCol.includes(currentIndex) === false && currentIndex >= 240 === false && (emptyCells.includes(cellBottomLeft) || numCells.includes(cellBottomLeft))) {
+        console.log(`We're checking ${currentIndex} for cellBottomLeft which is ${cellBottomLeft}`)
+        document.getElementById(`cell${cellBottomLeft}`).click()
+        // document.querySelector(`#cell${cellBottomLeft}`).classList.remove('evencell', 'oddcell')
+        // document.querySelector(`#cell${cellBottomLeft}`).classList.add('visible_cell')
+    }
+
+    console.log("We're at the end of revealAdjEmpties")
 }
 
 const popSound = document.getElementById('pop_sound')
@@ -289,13 +300,37 @@ const balloon = document.querySelector('.balloon')
 
 cellDiv.forEach(hiddenCell => {
     hiddenCell.addEventListener('click', (e) => {
-        // console.log(e.target.children[0].tagName === 'img')
-        // console.log(e.target.tagName)
-        console.log(e.target.id)
-        // getAdjEmpties()
-        // document.getElementById(`cell${i}`).click();
         let cellID = e.target.id
-        console.log(cellID)
+
+        if (cellID.length === 5) {
+            // Extracts last character (e.g. index)
+            // Note this is a string without parseInt()
+            let currentIndex = parseInt(cellID.slice(-1))
+
+            console.log("We're in cellID length 5")
+
+            if (emptyCells.includes(currentIndex)) {
+                revealAdjEmpties(currentIndex)
+            }
+        } else if (cellID.length === 6) {
+            // Extracts last 2 characters etc
+            let currentIndex = parseInt(cellID.slice(-2))
+
+            console.log("We're in cellID length 6")
+            
+            if (emptyCells.includes(currentIndex)) {
+                revealAdjEmpties(currentIndex)
+            }
+        } else if (cellID.length === 7) {
+            let currentIndex = parseInt(cellID.slice(-3))
+
+            console.log("We're in cellID length 7")
+            
+            if (emptyCells.includes(currentIndex)) {
+                revealAdjEmpties(currentIndex)
+            }
+        }
+
         if (e.target.tagName === 'IMG') {
             e.target.parentNode.classList.remove('evencell', 'oddcell')
             e.target.parentNode.classList.add('visible_cell')
